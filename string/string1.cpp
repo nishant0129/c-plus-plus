5ca0f49259767b6b4068fe3ddf97b9148b6ad863
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2021-08-12 16:21:30 +0530

    Add files via upload

---
+++ string/string1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char greeting[43]={'h','e','l','l','o'};
+    cout << "greeting message"<<endl;
+    cout << greeting<<endl;
+    return 0;
+
+}
